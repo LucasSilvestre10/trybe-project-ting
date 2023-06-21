56e31280f27df58fd6795cc69c4137f260114cbc
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-06-20 22:04:27 -0300

    requisitos 3,4,5 e 6 feitos

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,26 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    priority_queue.enqueue({"qtd_linhas": 10})
+    priority_queue.enqueue({"qtd_linhas": 3})
+    priority_queue.enqueue({"qtd_linhas": 7})
+
+    assert len(priority_queue) == 3
+
+    assert priority_queue.search(0)["qtd_linhas"] == 3
+    assert priority_queue.search(1)["qtd_linhas"] == 10
+    assert priority_queue.search(2)["qtd_linhas"] == 7
+
+    priority_queue.dequeue()
+    assert len(priority_queue) == 2
+
+    assert priority_queue.search(0)["qtd_linhas"] == 10
+    assert priority_queue.search(1)["qtd_linhas"] == 7
+
+    with pytest.raises(IndexError):
+        priority_queue.search(100)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,33 @@
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    new_queue = {
+        "nome_do_arquivo": str(path_file),
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+    if new_queue not in instance.queue_list:
+        print(new_queue)
+        instance.enqueue(new_queue)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance.queue_list) == 0:
+        print("Não há elementos", file=sys.stdout)
+        return
+    print(
+        f"Arquivo {instance.queue_list[0]['nome_do_arquivo']}",
+        "removido com sucesso",
+        file=sys.stdout,
+    )
+    instance.dequeue()
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position < 0 or position > len(instance.queue_list) - 1:
+        print("Posição inválida", file=sys.stderr)
+        return
+    print(instance.search(position))
