22f82d5acf4baddf28a901b0f9edff4e309483e6
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-06-20 21:42:10 -0300

    requisitos 1 e 2 feitos

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.isfile(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return
+
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return
+
+    with open(path_file, "r") as file:
+        lines = file.read().split("\n")
+
+    return lines
